1ee9b05845094e9fb6c6c4e39f90edb07e0ee110
U-HU0910\Hu <hu0910@gmail.com>
2016-02-10 10:48:06 -0500

    Auth(uncompleted) added

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -1,3 +1,14 @@
 /**
  * Created by Leon on 2016/2/5.
  */
+(function() {
+    'use strict';
+
+    angular.module('naf', [
+        'ngRoute', 'naf.presenter', 'naf.teacher', 'naf.course', 'naf.lecture', 'naf.attendee',
+        'naf.auth'])
+
+
+
+
+})();
