b701189391cbab8d7039ac61bd1c45b92aec8664
U-HU0910\Hu <hu0910@gmail.com>
2016-02-11 10:00:51 -0500

    fix conflict

--- app/js/auth/auth.controllers.js
+++ app/js/auth/auth.controllers.js
@@ -18,16 +18,17 @@
                     console.log(data);
                     $location.path('/register').replace();
                 }, function(err){
-                    console.log(err);
+                    $scope.users.error = err;
+                    console.log($scope.users.error);
                 })
         }
 
         function check(email, password){
             var defereed = $q.defer();
             if(email == '123@gmail.com' && password == '123456'){
-                defereed.resolve('Yes');
+                defereed.resolve('Login');
             } else{
-                defereed.reject('No');
+                defereed.reject('Invalid Email Or Passwords');
             }
             return defereed.promise;
         }
